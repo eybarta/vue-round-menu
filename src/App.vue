d2a19174f8266861e733b3f890290e568e310cab
Eyal Barta <eyalb@MIS-DOM.local>
2018-10-02 09:53:40 +0300

    updates

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<round-menu bg-color-closed="#ff6600"></round-menu>
+	</div>
+</template>
+
+<script>
+import RoundMenu from './RoundMenu.vue';
+export default {
+	components: {
+		RoundMenu
+	}
+}
+</script>
+<style lang="stylus">
+
+</style>
+
--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
 import Vue from 'vue';
-import RoundMenu from './RoundMenu.vue';
+import App from './App.vue';
 
 Vue.config.productionTip = false;
 
 new Vue({
-	render: (h) => h(RoundMenu)
+	render: (h) => h(App)
 }).$mount('#app');
-
